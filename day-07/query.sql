4412b554c98d12b37cd11b356dae7fd3289c7387
Shubham Kumar <145495342+Shubhthakur42@users.noreply.github.com>
2026-04-24 18:39:22 +0530

    Create tables for cards, stations, and swipes

--- day-07/query.sql
+++ day-07/query.sql
@@ -52,3 +52,23 @@ ALTER TABLE "swipes" RENAME COLUMN "ttpe" to "type";
 DROP table "stations";
 DROP TABLE "swipes";
 .schema
+CREATE TABLE "cards" (
+    "id" INTEGER,
+    PRIMARY KEY("id"));
+CREATE TABLE "stations" (
+    "id" INTEGER,
+    "name" TEXT NOT NULL UNIQUE,
+    PRIMARY KEY ("id")
+);
+CREATE TABLE "swipes" (
+    "id" INTEGER,
+    "card_id" INTEGER,
+    "station_id" INTEGER,
+    "type" TEXT NOT NULL CHECK ("type" IN ('enter','exit','deposit')),
+    "datetime" NUMERIC NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "amount" NUMERIC NOT NULL CHECK("amount" !=0),
+    PRIMARY KEY ("id"),
+    FOREIGN KEY ("card_id") REFERENCES "cards"("id"),
+    FOREIGN KEY ("station_id") REFERENCES "stations"("id")
+);
+
