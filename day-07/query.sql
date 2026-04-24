31c18831c3c767206b92e0eddb6892b548c21ec9
Shubham Kumar <145495342+Shubhthakur42@users.noreply.github.com>
2026-04-24 18:38:41 +0530

    Define riders, stations, and swipes tables with constraints
    Recreated tables with primary and foreign key constraints, added column constraints, and renamed visits table to swipes.

---
+++ day-07/query.sql
@@ -0,0 +1,54 @@
+-- Schema
+CREATE TABLE "riders" ("id","name");
+CREATE TABLE "stations" ("id","name","Line");
+-- Now we have to relate these two tables in visits table
+CREATE TABLE "visits" ("rider_id","station_id");
+-- it means rider_id from riders table relate with station_id from station table
+-- let us delete the tables
+DROP TABLE "riders";
+DROP TABLE "stations";
+
+DROP TABLE "visits";
+CREATE TABLE "riders" (
+    "id"  INTEGER,
+    "name" TEXT);
+CREATE TABLE "stations" (
+    "id" INTEGER,
+    "name" TEXT,
+    "line" TEXT);
+CREATE TABLE "visits" (
+    "rider_id" INTEGER,
+    "station_id" INTEGER );
+DROP TABLE "riders";
+DROP TABLE "stations";
+DROP TABLE "visits";
+.schema
+
+-- Recreating the table with primary key and foreign key with column constraints 1.primary key 2.foreign key 3. column constraint like not null and unique 
+
+CREATE TABLE "riders" (
+    "id" INTEGER,
+    "name" TEXT,
+    PRIMARY KEY("id")
+);
+CREATE TABLE "stations" (
+    "id" INTEGER,
+    "name" TEXT NOT NULL UNIQUE,
+    "line" TEXT NOT NULL,
+    PRIMARY KEY("id")
+);
+CREATE TABLE "visits" (
+    "riders_id" INTEGER,
+    "stations_id" INTEGER,
+    FOREIGN KEY ("riders_id") REFERENCES "riders"("id"),
+    FOREIGN KEY("stations_id") REFERENCES "stations"("id")
+);
+Drop table "riders";
+// rename visits  table t0 swipe
+ALTER TABLE "visits" RENAME TO "swipes";
+ALTER TABLE "swipes" ADD COLUMN "ttpe" TEXT;
+// By mistake i write ttpe of type so let correct It 
+ALTER TABLE "swipes" RENAME COLUMN "ttpe" to "type";
+DROP table "stations";
+DROP TABLE "swipes";
+.schema
