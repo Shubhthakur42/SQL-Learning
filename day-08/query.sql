ce1191a9625e8f62c3c1adbaf8c819808d75da17
Shubham Kumar <145495342+Shubhthakur42@users.noreply.github.com>
2026-04-25 12:32:43 +0530

    Document CSV import process in query.sql
    Added SQL comments and instructions for importing CSV data into the database.

--- day-08/query.sql
+++ day-08/query.sql
@@ -13,3 +13,16 @@ INSERT INTO "collections" ("title", "accession_number", "acquired")
 VALUES 
 ('Imaginative landscape', '56.496', NULL),
 ('Peonies and butterfly', '06.1899', '1906-01-01');
+-- How to import a csv file directly into database
+-- museum.csv
+title,accession_number,acquired
+Profusion of flowers,56.257,1956-04-12
+Farmers working at dawn,11.6152,1911-08-03
+Spring outing,14.76,1914-01-08
+Imaginative landscape,56.496,
+Peonies and butterfly,06.1899,1906-01-01
+-- first make temp file and store all the value then copy all the details into collections table
+.import --csv mfa.csv temp
+-- then
+INSERT INTO "collections" ("title", "accession_number", "acquired") 
+SELECT "title", "accession_number", "acquired" FROM "temp";
